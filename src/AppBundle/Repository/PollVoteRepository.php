6c354b47e417a63071bd7c3be7103ffe844a3e06
Dawid Parafiński <dawid.parafinski@ez.no>
2018-08-17 15:32:26 +0200

    [STEP-5] show results of single poll

--- src/AppBundle/Repository/PollVoteRepository.php
+++ src/AppBundle/Repository/PollVoteRepository.php
@@ -25,4 +25,15 @@ final class PollVoteRepository
             )
             ->getResult();
     }
+
+    public function findAnswersByFieldId(int $fieldId, int $contentId)
+    {
+        $query = $this->entityManager
+            ->createQuery(
+                'SELECT p FROM AppBundle:PollVote p WHERE p.fieldId = :fieldId ORDER BY p.id ASC'
+            );
+        $query->setParameter('fieldId', $fieldId);
+
+        return $query->getResult();
+    }
 }
