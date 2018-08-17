05515b8bb88b538f73f32d10f25c57753579edfa
Dawid Parafiński <dawid.parafinski@ez.no>
2018-08-17 14:19:32 +0200

    [STEP-2] Handle Poll on frontend

---
+++ src/AppBundle/Entity/PollVote.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PollVoteRepository")
+ */
+class PollVote
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $answer;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\NotNull()
+     */
+    private $fieldId;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\NotNull()
+     */
+    private $contentId;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    /**
+     * @param mixed $question
+     */
+    public function setQuestion($question): void
+    {
+        $this->question = $question;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
+
+    /**
+     * @param mixed $answer
+     */
+    public function setAnswer($answer): void
+    {
+        $this->answer = $answer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFieldId()
+    {
+        return $this->fieldId;
+    }
+
+    /**
+     * @param mixed $fieldId
+     */
+    public function setFieldId($fieldId): void
+    {
+        $this->fieldId = $fieldId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContentId()
+    {
+        return $this->contentId;
+    }
+
+    /**
+     * @param mixed $contentId
+     */
+    public function setContentId($contentId): void
+    {
+        $this->contentId = $contentId;
+    }
+
+}
