8ddd2716e8b759932f16519a87601b52af36e54c
Dawid Parafiński <dawid.parafinski@ez.no>
2018-08-17 15:25:35 +0200

    [STEP-4] Add listing of all polls

--- src/AppBundle/Repository/PollVoteRepository.php
+++ src/AppBundle/Repository/PollVoteRepository.php
@@ -2,6 +2,27 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\EntityManager;
+
 final class PollVoteRepository
 {
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function findAllOrderedByQuestion()
+    {
+        return $this->entityManager
+            ->createQuery(
+                'SELECT DISTINCT p.fieldId, p.contentId, p.question FROM AppBundle:PollVote p ORDER BY p.question ASC'
+            )
+            ->getResult();
+    }
 }
